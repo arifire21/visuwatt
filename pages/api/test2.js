523e3dad98b00605abcc7c441f4a5f0b894cddb2
arifire21 <ariwolfpup@icloud.com>
2024-10-06 04:12:16 -0400

    OMG IT WORKS

--- pages/api/test2.js
+++ pages/api/test2.js
@@ -1,9 +1,41 @@
-import clientPromise from "@/lib/mongodb";
+import client from "@/lib/mongodb";
 
 export default async(req, res) => {
-    const mongoClient = await clientPromise;
+    // try {
+    const mongoClient = await client;
+    // console.log('MGO CLIENT')
+    // console.log(mongoClient)
+    let connectedClient = await mongoClient.connect();
+    // console.log('connectedCLIENT')
+    // console.log(connectedClient)
+    const db = connectedClient.db('Counties')
 
-    const data = await mongoClient.db().collection('Broward').find().toArray()
+    // console.log(db)
+    console.log('hi')
 
-    res.status(200).json(JSON.parse(JSON.stringify(data)));
+    const data = await db.collection('Broward')
+        .find({})
+        .toArray();
+    
+    res.json(data)
+    // res.status(200).json({test:'test'})
 }
+    // const movies = await db
+    //     .collection(movies)
+    //     .find({})
+    //     .limit(10)
+    //     .toArray();
+
+    // res.json(movies);
+    // }   catch(e) {
+    //     console.log(e)
+    // }
+
+    // Get a list of 50 posts
+// router.get("/", async (req, res) => {
+//     let collection = await db.collection("posts");
+//     let results = await collection.find({})
+//       .limit(50)
+//       .toArray();
+//     res.send(results).status(200);
+//   });
