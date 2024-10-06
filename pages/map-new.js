6cf222f6fc739c88be51ec20523f952155504a6a
arifire21 <ariwolfpup@icloud.com>
2024-10-06 05:27:55 -0400

    api linked with bubble click function

--- pages/map-new.js
+++ pages/map-new.js
@@ -2,90 +2,118 @@ import { Map, Marker, Overlay } from "pigeon-maps"
 import Image from 'next/image'
 import { useState } from "react"
 import { Button } from "@mui/joy";
+import {collections} from '../lib/collection-names'
 
 export default function MapNew() {
-    const [bubble, setBubble] = useState({ visible: false, position: [0,0], text: ''});
+  const [bubble, setBubble] = useState({ visible: false, position: [0, 0], text: '' });
+  const [requests, setRequests] = useState([])
 
-    const handleMarkerClick = (position, text) => {
-        if (bubble.visible && bubble.position[0] === position[0]) {
-            // If the same marker is clicked again, hide the bubble
-            setBubble({ visible: false, position: [0, 0], text: '' });
-        } else {
-            // Otherwise, show the bubble with the new position and text
-            setBubble({ visible: true, position, text });
-        }
+  const handleMarkerClick = (position, text, queryStr) => {
+    if (bubble.visible && bubble.position[0] === position[0]) {
+      // If the same marker is clicked again, hide the bubble
+      setBubble({ visible: false, position: [0, 0], text: '' });
+    } else {
+      // Otherwise, show the bubble with the new position and text
+      setBubble({ visible: true, position, text });
+
+      //call API after showing results? for now.
+      //TODO: replace this with a LoadingCircle while the API is fetching then rendering data
+      getRecords(queryStr)
     }
+  }
 
-    return (
-        <Map height={700} defaultCenter={[27.9659, -82.7959]} defaultZoom={7} zoom={7}>
-        {/* Orange County */}
-        <Marker width={50} anchor={[28.5384, -81.3789]} color={'red'} onClick={() => handleMarkerClick([28.5384, -81.3789], "Orange County")}/>
+  function dataHelper(data) { //relying on state in getData does not work because of state's delayed updating
+    console.log(`helper data print: ${data}`)
+    setRequests(data)
+  }
 
-        {/* Broward */}
-        <Marker width={50} anchor={[26.1901, -80.3659]} color={'red'} onClick={() => handleMarkerClick([26.1901, -80.3659], "Broward County")}/>
+  const getRecords = async (bubbleName) => {
+    // try {
+    const req = await fetch(`/api/find-record-from-county?county=${bubbleName}`, {
+      method: "GET",
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+      }
+    })
+      .then((res) => {
+        if (!res.ok) {
+          console.log('API ERROR')
+          return null;
+        }
+        else {
+          return res.json() // Parse the response data as JSON
+        }
+      })
+      .then((data) => { dataHelper(data) })
+      .catch(err => console.log(err));
+  }
 
-        {/* Palm Beach */}
-        <Marker width={50} anchor={[26.6515, -80.2767]} color={'red'} onClick={() => handleMarkerClick([26.6515, -80.2767], "Palm Beach County")}/>
+  return (
+    <Map height={700} defaultCenter={[27.9659, -82.7959]} defaultZoom={7} zoom={7} mouseEvents={false} touchEvents={false}>
+      {/* Orange County */}
+      <Marker width={50} anchor={[28.5384, -81.3789]} color={'red'} onClick={() => handleMarkerClick([28.5384, -81.3789], "Orange County", collections[7])} />
 
-        {/* Hillsborough */}
-        <Marker width={50} anchor={[27.9904, -82.3018]} color={'red'} onClick={() => handleMarkerClick([27.9904, -82.3018], "Hillsborough County")}/>
+      {/* Broward */}
+      <Marker width={50} anchor={[26.1901, -80.3659]} color={'red'} onClick={() => handleMarkerClick([26.1901, -80.3659], "Broward County", collections[0])} />
 
-        {/* Miami - Dade */}
-        <Marker width={50} anchor={[25.7617, -80.1918]} color={'red'} onClick={() => handleMarkerClick([25.7617, -80.1918], "Miami-Dade County")}/>
+      {/* Palm Beach */}
+      <Marker width={50} anchor={[26.6515, -80.2767]} color={'red'} onClick={() => handleMarkerClick([26.6515, -80.2767], "Palm Beach County", collections[8])} />
 
-        {/* Franklin */}
-        <Marker width={50} anchor={[29.8503, -84.8723]} color={'blue'} onClick={() => handleMarkerClick([29.8503, -84.8723], "Franklin County")}/>
+      {/* Hillsborough */}
+      <Marker width={50} anchor={[27.9904, -82.3018]} color={'red'} onClick={() => handleMarkerClick([27.9904, -82.3018], "Hillsborough County", collections[2])} />
 
-        {/* Lafayette */}
-        <Marker width={50} anchor={[29.9844, -83.1766]} color={'blue'} onClick={() => handleMarkerClick([29.9844, -83.1766], "Lafayette County")}/>
+      {/* Miami - Dade */}
+      <Marker width={50} anchor={[25.7617, -80.1918]} color={'red'} onClick={() => handleMarkerClick([25.7617, -80.1918], "Miami-Dade County", collections[5])} />
 
-        {/* Union */}
-        <Marker width={50} anchor={[30.0454, -82.3624]} color={'blue'} onClick={() => handleMarkerClick([30.0454, -82.3624], "Union County")}/>
+      {/* Franklin */}
+      <Marker width={50} anchor={[29.8503, -84.8723]} color={'blue'} onClick={() => handleMarkerClick([29.8503, -84.8723], "Franklin County", collections[1])} />
 
-        {/* Monroe */}
-        <Marker width={50} anchor={[25.2974, -81.0755]} color={'blue'} onClick={() => handleMarkerClick([25.2974, -81.0755], "Monroe County")}/>
+      {/* Lafayette */}
+      <Marker width={50} anchor={[29.9844, -83.1766]} color={'blue'} onClick={() => handleMarkerClick([29.9844, -83.1766], "Lafayette County", collections[3])} />
 
-        {/* Liberty */}
-        <Marker width={50} anchor={[30.1508, -84.8568]} color={'blue'} onClick={() => handleMarkerClick([30.1508, -84.8568], "Liberty County")}/>
+      {/* Union */}
+      <Marker width={50} anchor={[30.0454, -82.3624]} color={'blue'} onClick={() => handleMarkerClick([30.0454, -82.3624], "Union County", collections[9])} />
 
-        {/* Legend */}
-        <Overlay anchor={[26.2333, -86.8833]} offset={[0, 0]}>
+      {/* Monroe */}
+      <Marker width={50} anchor={[25.2974, -81.0755]} color={'blue'} onClick={() => handleMarkerClick([25.2974, -81.0755], "Monroe County", collections[6])} />
+
+      {/* Liberty */}
+      <Marker width={50} anchor={[30.1508, -84.8568]} color={'blue'} onClick={() => handleMarkerClick([30.1508, -84.8568], "Liberty County", collections[4])} />
+
+      {/* Legend */}
+      <Overlay anchor={[26.2333, -86.8833]} offset={[0, 0]}>
         <Image src="/Legend.jpeg" width={240} height={158} alt='HI' />
+      </Overlay>
+
+      {/* Survey Button */}
+      <Overlay anchor={[27.0, -86.8833]} offset={[0, 0]}>
+        <Button
+          variant="solid"
+          color="success"
+          onClick={() => console.log("Survey Button Clicked")}
+        >
+          Take Survey
+        </Button>
+      </Overlay>
+
+      {/* Text Bubble */}
+      {bubble.visible && (
+        <Overlay anchor={bubble.position} offset={[0, 0]}>
+          <div style={bubbleStyle}>
+            {bubble.text}
+          </div>
         </Overlay>
-
-        {/* Survey Button */}
-            <Overlay anchor={[27.0, -86.8833]} offset={[0, 0]}>
-                <Button
-                    variant="contained"
-                    color="primary"
-                    onClick={() => console.log("Survey Button Clicked")}
-                    sx={{
-                        backgroundColor: 'green',
-                    }}
-                >
-                    Take Survey
-                </Button>
-            </Overlay>
-
-        {/* Text Bubble */}
-        {bubble.visible && (
-                    <Overlay anchor={bubble.position} offset={[0, 0]}>
-                        <div style={bubbleStyle}>
-                            {bubble.text}
-                        </div>
-                    </Overlay>
-        
-
-        )}
-      </Map>
-    );
+      )}
+    </Map>
+  );
 }
 
 const bubbleStyle = {
-    backgroundColor: 'white',
-    padding: '5px 10px',
-    borderRadius: '5px',
-    boxShadow: '0 0 10px rgba(0,0,0,0.1)',
-    whiteSpace: 'nowrap',
-    color: 'black',
+  backgroundColor: 'white',
+  padding: '5px 10px',
+  borderRadius: '5px',
+  boxShadow: '0 0 10px rgba(0,0,0,0.1)',
+  whiteSpace: 'nowrap',
+  color: 'black',
 };
